f7506210270e8da6b15af2f2f3e306f960575355
albertbrasi <albertbrasi001@gmail.com>
2022-07-02 18:53:07 +0700

    Video#34 Sử Dụng State và Cách Xử Lý Sự Kiện Event OnClick, OnChange với React

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -9,7 +9,35 @@ import { FormattedMessage } from 'react-intl';
 class Login extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            username: '',
+            password: '',
+            isShowPassword: false
+        }
     }
+
+    handleOnChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleLogin = () => {
+
+    }
+
+    handleShowHidePassword = () => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword
+        })
+    }
+
     render() {
 
 
@@ -20,14 +48,26 @@ class Login extends Component {
                         <div className='col-12 text-login'>Login</div>
                         <div className='col-12 form-group login-input'>
                             <label>Username:</label>
-                            <input type='text' className='form-control' placeholder='Enter your username' />
+                            <input type='text' className='form-control' placeholder='Enter your username'
+                                value={this.state.username}
+                                onChange={(event) => this.handleOnChangeUsername(event)} />
                         </div>
                         <div className='col-12 form-group login-input'>
                             <label>Password:</label>
-                            <input type='password' className='form-control' placeholder='Enter your password' />
+                            <div className='custom-input-password'>
+                                <input
+                                    type={this.state.isShowPassword ? 'text' : 'password'}
+                                    className='form-control' placeholder='Enter your password'
+                                    value={this.state.password}
+                                    onChange={(event) => this.handleOnChangePassword(event)} />
+                                <span
+                                    onClick={() => this.handleShowHidePassword()}>
+                                    <i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
+                                </span>
+                            </div>
                         </div>
                         <div className='col-12'>
-                            <button className='btn-login'>Login</button>
+                            <button className='btn-login' onClick={() => this.handleLogin()}>Login</button>
                             <div className='col-12'>
                                 <span className='forgot-password'>Forgot your password?</span>
                             </div>
@@ -42,7 +82,7 @@ class Login extends Component {
 
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
