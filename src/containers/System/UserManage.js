10f720cab12fe5010e79dd7eb7b58b4aa28cd6bc
albertbrasi <albertbrasi001@gmail.com>
2022-07-12 07:36:17 +0700

    Video#40 CRUD React 4 - Design Modal Thêm Mới Người Dùng Với Reactstrap

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,13 +3,15 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss'
 import { getAllUsers } from '../../services/userService'
+import ModalUser from './ModalUser';
 
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
@@ -28,13 +30,37 @@ class UserManage extends Component {
         // console.log('data: ', response)
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser
+        })
+    }
+
 
     render() {
         let arrUsers = this.state.arrUsers
         console.log('data kkldsfjlsdf: ', arrUsers)
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test={'abc'}
+                />
                 <div className='title text-center'>Manage users</div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3'
+                        onClick={() => this.handleAddNewUser()}>
+                        <i className="fas fa-plus"></i>
+                        Add new user
+                    </button>
+                </div>
                 <div className='users-table'>
                     <table>
                         <tr>
