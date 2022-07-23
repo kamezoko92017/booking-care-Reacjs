e5ac9b263172110356f5437bd57d035faebd1d22
albertbrasi <albertbrasi001@gmail.com>
2022-07-23 23:07:14 +0700

    Video#55 Hoàn Thiện Menu Phân Quyền Dành Cho Admin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils'
+import { FormattedMessage } from 'react-intl'
 
 class Header extends Component {
 
@@ -14,7 +15,8 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
+        console.log('userinfo: ', userInfo)
 
         return (
             <div className="header-container">
@@ -24,6 +26,10 @@ class Header extends Component {
                 </div>
 
                 <div className='languages'>
+                    <span className='welcom'>
+                        <FormattedMessage id="home-header.welcom" />
+                        {userInfo && userInfo.lastName ? userInfo.lastName : ''}
+                        !</span>
                     <span className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
                     >VN</span>
@@ -46,6 +52,7 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
+        userInfo: state.user.userInfo,
         language: state.app.language,
     };
 };
